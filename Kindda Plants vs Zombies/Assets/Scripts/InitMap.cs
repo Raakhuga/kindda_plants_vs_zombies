bda3eee5f7cdc1dcac2b86b9a53bf1a57e70a0f4
adrian.sanchez.albanell <adrian.sanchez.albanell@fib.upc.edu>
2019-04-19 22:00:16 +0200

    create auto map tiles, refactor raycast and rename MousePointer

---
+++ Kindda Plants vs Zombies/Assets/Scripts/InitMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitMap : MonoBehaviour
+{
+    public GameObject Tile1;
+    public GameObject Tile2;
+    private GameObject[] map;
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = new GameObject[45];
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                GameObject TyleType = Tile1;
+                if ((i + j) % 2 == 0) TyleType = Tile2;
+                map[i * 5 + j] = Instantiate(TyleType, new Vector3(i, 0.0f, j), Quaternion.identity) as GameObject;
+                map[i * 5 + j].tag = "Tile";
+                map[i * 5 + j].name = "tile " + i + " " + j;
+            }
+        }
+    }
+}
---
+++ Kindda Plants vs Zombies/Assets/Scripts/MousePointer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class MousePointer : MonoBehaviour
+{
+
+    Shader shader;
+    Shader hoverShader;
+    private Renderer currentTile;
+
+    void Start()
+    {
+        shader = Shader.Find("Standard");
+        hoverShader = Shader.Find("Custom/HoverObject");
+        currentTile = null;
+    }
+
+    public void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.magenta);
+
+        RaycastHit[] hits = Physics.RaycastAll(ray.origin, ray.direction, 100.0F);
+
+        // Search if we hit a tile
+        if (currentTile) currentTile.material.shader = shader; // reset currentTile (if we hit nothing, clean last tile selected)
+        foreach (RaycastHit hit in hits)
+        {
+            Renderer rend = hit.transform.GetComponent<Renderer>();
+            if (rend.CompareTag("tile"))
+            {
+                if (currentTile) currentTile.material.shader = shader;
+                currentTile = rend;
+
+                rend.material.shader = hoverShader;
+
+                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red);
+            }
+        }
+    }
+}
