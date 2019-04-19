6bb6e9743f25018ac0f8823f03c69f70495b25ac
adrian.sanchez.albanell <adrian.sanchez.albanell@fib.upc.edu>
2019-04-19 21:57:27 +0200

    try raycast and mouseOver

---
+++ Kindda Plants vs Zombies/Assets/Scripts/RayCast.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+
+    Shader shader;
+    Shader hoverShader;
+
+    void Start()
+    {
+        shader = Shader.Find("Standard");
+        hoverShader = Shader.Find("Custom/HoverObject");
+    }
+
+    public void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.magenta);
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit) == true)
+        {
+            if (hit.transform.gameObject.CompareTag("tile"))
+            {
+                hit.transform.gameObject.GetComponent<Renderer>().material.shader = hoverShader;
+                Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red);
+            }
+            Debug.Log("Hit " + hit.transform.gameObject.tag);
+        }
+
+    }
+}
---
+++ Kindda Plants vs Zombies/Assets/Scripts/mouseOver.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class mouseOver : MonoBehaviour
+{
+    Shader shader;
+    Shader hoverShader;
+    bool hover = false;
+
+    void Start()
+    {
+        shader = Shader.Find("Standard");
+        hoverShader = Shader.Find("Custom/HoverObject");
+    }
+
+    void OnMouseOver()
+    {
+        hover = true;
+        if (GetComponent<Renderer>().CompareTag("tile"))
+        {
+            GetComponent<Renderer>().material.shader = hoverShader;
+            Debug.Log("Mouse is over a tile.");
+        }
+    }
+
+    void OnMouseExit()
+    {
+        hover = false;
+        if (GetComponent<Renderer>().CompareTag("tile"))
+        {
+            GetComponent<Renderer>().material.shader = shader;
+            Debug.Log("Mouse is no longer on tile.");
+        }
+    }
+}
