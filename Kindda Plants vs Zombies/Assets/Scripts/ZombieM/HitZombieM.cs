63ecd34dc1b4715f6c07b4d76069bebc26e14259
Unknown <santirubiomontilla@gmail.com>
2019-04-19 19:13:23 +0200

    added zombie with some animations
     - walking animation moves the zombie, so it's model is outside its real position
     - dmg works both sides

---
+++ Kindda Plants vs Zombies/Assets/Scripts/ZombieM/HitZombieM.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitZombieM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay (Collider col) 
+    {
+		if (col.gameObject.CompareTag("ally"))
+		{
+			transform.parent.gameObject.GetComponent<ZombieMCtrl>().attack(col.gameObject.transform.parent.gameObject);
+		}    	
+    }
+}
---
+++ Kindda Plants vs Zombies/Assets/Scripts/ZombieM/ZombieMCtrl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMCtrl : MonoBehaviour
+{
+	private Stats s;
+	private bool canMove;
+	private Animator anim;
+	private float nextAttack;
+	private GameObject col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	canMove = true;
+        s = GetComponent<Stats>();
+        anim = GetComponent<Animator>();
+    	anim.SetBool("CanMove", true);
+    	nextAttack = Time.time + s.AttackSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canMove) transform.Translate(Vector3.forward * s.vel * Time.deltaTime);
+    }
+
+    public void attack(GameObject col) 
+    {
+    	if (Time.time > nextAttack) {
+				canMove=false;
+				anim.SetBool("CanMove", false);
+				anim.SetBool("Attack", true);
+				this.col = col;
+				nextAttack = Time.time + s.AttackSpeed + 100;  	
+	    }
+	}
+
+    void resetAttck() 
+    {
+    	anim.SetBool("Attack", false);
+    	nextAttack = Time.time + s.AttackSpeed;
+    	col.GetComponent<Health>().health -= s.dmg;
+		if (col.GetComponent<Health>().health <= 0) canMove = true;
+		this.col = null;
+    }
+}
