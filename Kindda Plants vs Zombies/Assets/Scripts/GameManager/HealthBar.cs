a82362e6b6115ed3c1efd8957c4859b7c9a842bf
Unknown <santirubiomontilla@gmail.com>
2019-05-03 01:16:58 +0200

    Bars facing correct way

--- Kindda Plants vs Zombies/Assets/Scripts/GameManager/HealthBar.cs
+++ Kindda Plants vs Zombies/Assets/Scripts/GameManager/HealthBar.cs
@@ -9,41 +9,52 @@ public class HealthBar : MonoBehaviour
 	private Image HBfill;
 	private GameObject newHBar;
 	private float InitialHealth;
+	private float currentHealth;
 	private Stats sts;
+	private Camera mainCamera;
+	private bool damaging;
     // Start is called before the first frame update
     void Start()
     {
         sts = GetComponent<Stats>();
-        InitialHealth = sts.health; 
-               
+        InitialHealth = sts.health;
+        currentHealth = InitialHealth;
+        mainCamera = Camera.main;
+        Vector3 pos = new Vector3(transform.position.x, 2.5f, transform.position.z);
+		newHBar = Instantiate(HBar, pos, transform.rotation);
+		newHBar.transform.parent = transform;
+		Vector3 camPos = new Vector3(transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z);
+		newHBar.transform.LookAt(camPos);
+		newHBar.SetActive(false);
+		HBfill = newHBar.transform.Find("HealthBar_full").gameObject.GetComponent<Image>();
+		damaging = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         if (sts.health != InitialHealth){
-        	if(newHBar == null) {
-        		Vector3 pos = new Vector3(transform.position.x, 2.5f, transform.position.z);
-        		newHBar = Instantiate(HBar, pos, transform.rotation);
-        		newHBar.transform.parent = transform;
-        		HBfill = newHBar.transform.Find("HealthBar_full").gameObject.GetComponent<Image>();
+        	if (!newHBar.activeSelf) {
+        		newHBar.SetActive(true);
         	}
         	float pct = sts.health / InitialHealth;
-        	StartCoroutine(changeValue(pct));
+        	if(!damaging && currentHealth != sts.health) StartCoroutine(changeValue(pct));
         }
     }
 
     private IEnumerator changeValue(float pct) 
     {
+    	damaging = true;
     	float initPct = HBfill.fillAmount;
-    	float elapsed = 0f;
-    	float speed = 0.5f;
-
-    	while (elapsed < speed) {
-    		elapsed += Time.deltaTime;
+    	float speed = 0.2f;
+    	for(float elapsed = 0; elapsed < speed; elapsed += Time.deltaTime) {
+    		//elapsed += Time.deltaTime;
     		HBfill.fillAmount = Mathf.Lerp(initPct, pct, elapsed / speed);
+
     		yield return null;
     	}
-    	HBfill.fillAmount = pct;
+    	currentHealth = sts.health;
+    	damaging = false;
+    	//HBfill.fillAmount = pct;
     }
 }
