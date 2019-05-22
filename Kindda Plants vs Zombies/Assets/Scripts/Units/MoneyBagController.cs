471f2718795ebe40b433fb0ac01623a1ec63a6e7
Unknown <adrian.sal@gmail.com>
2019-05-22 15:48:56 +0200

    change camera a little, fix money bags "y" bug

--- Kindda Plants vs Zombies/Assets/Scripts/Units/MoneyBagController.cs
+++ Kindda Plants vs Zombies/Assets/Scripts/Units/MoneyBagController.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MoneyBagController : MonoBehaviour
 {
     public int gold = 25;
+
+    private float yMin = 0;
     void Update()
     {
-        if (transform.position.y > 0.35F) transform.Translate(new Vector3(0, -1, 0) * 4 * Time.deltaTime);
+        if (transform.position.y > yMin)
+        {
+            transform.Translate(new Vector3(0, -1, 0) * 4 * Time.deltaTime);
+            if (transform.position.y < yMin)
+            {
+                transform.SetPositionAndRotation(
+                    new Vector3(transform.position.x, yMin, transform.position.z),
+                    transform.rotation);
+            }
+        }
     }
 }
