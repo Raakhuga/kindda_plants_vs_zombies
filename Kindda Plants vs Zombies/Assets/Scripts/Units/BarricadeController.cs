6ed3bc18383494bd85227388f5e7ec664c7e9e70
Raakhuga <santirubiomontilla@gmail.com>
2019-05-26 22:23:13 +0200

    anim barricade

--- Kindda Plants vs Zombies/Assets/Scripts/Units/BarricadeController.cs
+++ Kindda Plants vs Zombies/Assets/Scripts/Units/BarricadeController.cs
@@ -3,9 +3,16 @@
 public class BarricadeController : MonoBehaviour
 {
     private Stats sts;
+    public GameObject anim;
 
     void Start()
     {
         sts = GetComponent<Stats>();
+    }
+
+    public void Die()
+    {
+        GameObject aux = Instantiate(anim, transform.position, transform.rotation);
+        aux.transform.parent = transform;
     }
 }
